233a54935dd4b03cf2edbc5d761a76bd710d6a4d
Marcos Dominguez <mrds0690@gmail.com>
2023-09-12 12:26:58 -0300

    feat: create ciaa project

---
+++ src/dominguez-iso1.c
@@ -0,0 +1,37 @@
+/*=============================================================================
+ * Author: Marcos Dominguez <mrds0690@gmail.com>
+ * Date: 2023/09/12
+ * Version: 1
+ *===========================================================================*/
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+#include "dominguez-iso1.h"
+#include "sapi.h"
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Definitions of extern global variables]==============================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Main function, program entry point after power on or reset]==========*/
+
+int main( void )
+{
+   // ----- Setup -----------------------------------
+   boardInit();
+
+   // ----- Repeat for ever -------------------------
+   while( true ) {
+      gpioToggle(LED);
+      delay(500);
+   }
+
+   // YOU NEVER REACH HERE, because this program runs directly or on a
+   // microcontroller and is not called by any Operating System, as in the 
+   // case of a PC program.
+   return 0;
+}

1caa027e78c4238a13688687d22df9baf382f298
Marcos Dominguez <mrds0690@gmail.com>
2023-10-04 00:08:42 -0300

    fix: check for null handler and callbacks at task creation

--- src/OS/os_kernel.c
+++ src/OS/os_kernel.c
@@ -112,43 +112,45 @@ __STATIC_FORCEINLINE void AsyncChangeOfContext(void);
 /* ================= Public functions implementation ================ */
 bool OS_KERNEL_TaskCreate(os_task_t *handler, os_priority_t priority, void *callback) {
     bool ret = false;
-    uint8_t i;
-    for (i = (IDLE_TASK_INDEX + 1); i < MAX_NUMBER_TASK; i++) { //find available mem region
-        if (os_kernel.list_task[i] == NULL) {
-            handler->id = (uintptr_t)&os_kernel.list_task[i]; //save the mem position of the element in the list task as ID
-            break;
+    if ((handler != NULL) && (callback != NULL)) {
+        uint8_t i;
+        for (i = (IDLE_TASK_INDEX + 1); i < MAX_NUMBER_TASK; i++) { //find available mem region
+            if (os_kernel.list_task[i] == NULL) {
+                handler->id = (uintptr_t)&os_kernel.list_task[i]; //save the mem position of the element in the list task as ID
+                break;
+            }
         }
-    }
-    if (i < MAX_NUMBER_TASK) { // check if there was memory available
-        ret = true;
-        // xPSR value with 24 bit on one (Thumb mode).
-        handler->memory[STACK_POS(XPSR_REG_POSITION)]   = XPSR_VALUE;
-        // Program pointer (PC) points to function used by the task.
-        handler->memory[STACK_POS(PC_REG_POSTION)]      = (uint32_t)callback;
-        handler->memory[STACK_POS(LR_REG_POSTION)]      = (uint32_t)OS_KERNEL_ReturnTaskHook;
+        if (i < MAX_NUMBER_TASK) { // check if there was memory available
+            ret = true;
+            // xPSR value with 24 bit on one (Thumb mode).
+            handler->memory[STACK_POS(XPSR_REG_POSITION)]   = XPSR_VALUE;
+            // Program pointer (PC) points to function used by the task.
+            handler->memory[STACK_POS(PC_REG_POSTION)]      = (uint32_t)callback;
+            handler->memory[STACK_POS(LR_REG_POSTION)]      = (uint32_t)OS_KERNEL_ReturnTaskHook;
 
-        /*
-         * Previous Link register (LR) value because handler pendSV call function inside exception
-         * and LR is overwrite with the return value of ChangeOfContext.
-         */
-        handler->memory[STACK_POS(LR_PREV_VALUE_POSTION)] = EXEC_RETURN_VALUE;
+            /*
+             * Previous Link register (LR) value because handler pendSV call function inside exception
+             * and LR is overwrite with the return value of ChangeOfContext.
+             */
+            handler->memory[STACK_POS(LR_PREV_VALUE_POSTION)] = EXEC_RETURN_VALUE;
 
-        // Pointer function of task.
-        handler->entry_point     = callback;
-        handler->stack_pointer   = (uint32_t)(handler->memory + MAX_TASK_SIZE - SIZE_STACK_FRAME);
-        handler->status         = OS_TASK_READY;
-        if (priority >= PRIORITY_LEVELS) {
-            handler->priority = PRIORITY_LEVELS - 1;
+            // Pointer function of task.
+            handler->entry_point     = callback;
+            handler->stack_pointer   = (uint32_t)(handler->memory + MAX_TASK_SIZE - SIZE_STACK_FRAME);
+            handler->status         = OS_TASK_READY;
+            if (priority >= PRIORITY_LEVELS) {
+                handler->priority = PRIORITY_LEVELS - 1;
+            }
+            else {
+                handler->priority       = priority;
+            }
+
+
+            // Fill controls OS structure
+            os_task_t **temp = (os_task_t **)handler->id;
+            *temp = (handler); //save the handler in the os_kernel.list_task[i].
+            PushTaskToWaitingList(handler);
         }
-        else {
-            handler->priority       = priority;
-        }
-
-
-        // Fill controls OS structure
-        os_task_t **temp = (os_task_t **)handler->id;
-        *temp = (handler); //save the handler in the os_kernel.list_task[i].
-        PushTaskToWaitingList(handler);
     }
 
     return ret;
